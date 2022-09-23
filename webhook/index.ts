d61478b66c35a5f5f5692cf0b59543bef7e475c2
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-09-23 21:50:10 +0900

    update webhook

--- webhook/index.ts
+++ webhook/index.ts
@@ -1,10 +1,21 @@
 import fastify from "fastify";
 import { $ } from "zx";
 
+const createRandomString = (length: number) => {
+  const S = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let rnd = "";
+  for (let i = 0; i < length; i++) {
+    rnd += S.charAt(Math.floor(Math.random() * S.length));
+  }
+  return rnd;
+};
+
 const app = fastify({ logger: true });
 
 app.get("/", async (request, reply) => {
-  await $`gcloud beta batch jobs submit download-job --location asia-southeast1 --config ./configs/download-job.json`;
+  await $`gcloud beta batch jobs submit download-job-${createRandomString(
+    16
+  )} --location asia-southeast1 --config ./configs/download-job.json`;
   reply.code(200).send({
     hello: "world",
     httpVersion: request.raw.httpVersion,
