48585604520a3856d4fab3683fb343255a3b5fc7
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-09-23 21:59:31 +0900

    update webhook

--- webhook/index.ts
+++ webhook/index.ts
@@ -2,7 +2,7 @@ import fastify from "fastify";
 import { $ } from "zx";
 
 const createRandomString = (length: number) => {
-  const S = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const S = "0123456789abcdefghijklmnopqrstuvwxyz";
   let rnd = "";
   for (let i = 0; i < length; i++) {
     rnd += S.charAt(Math.floor(Math.random() * S.length));
