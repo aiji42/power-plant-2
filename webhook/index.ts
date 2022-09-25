c0a3102cfd1276c0c8cb6c96ae5cbde25c393e3c
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-09-25 17:33:08 +0900

    add endpoint for deleting a media

--- webhook/index.ts
+++ webhook/index.ts
@@ -1,5 +1,15 @@
 import fastify from "fastify";
 import { $ } from "zx";
+import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
+
+const S3 = new S3Client({
+  region: "auto",
+  endpoint: `https://${process.env.R2_CLIENT_ID}.r2.cloudflarestorage.com`,
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY_ID,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+  },
+});
 
 const createRandomString = (length: number) => {
   const S = "0123456789abcdefghijklmnopqrstuvwxyz";
@@ -13,32 +23,46 @@ const createRandomString = (length: number) => {
 const app = fastify({ logger: true });
 
 type SupabaseWebhookData<
-  T extends Record<string, unknown> = Record<string, any>
+  R extends Record<string, unknown> | null,
+  O extends Record<string, unknown> | null
 > = {
-  data: {
-    type: "DELETE" | "INSERT" | "UPDATE";
-    table: string;
-    record: null | unknown;
-    schema: string;
-    old_record: T;
-  };
+  type: "DELETE" | "INSERT" | "UPDATE";
+  table: string;
+  record: R;
+  schema: string;
+  old_record: O;
 };
 
 app.post("/download-task", async (request, reply) => {
-  const log =
-    await $`gcloud beta batch jobs submit download-job-${createRandomString(
-      16
-    )} --location asia-southeast1 --config ./configs/download-job.json`;
-  reply.code(200).send({ message: log.message, stdout: log.stdout });
+  const data = request.body as SupabaseWebhookData<{ id: string }, null>;
+  if (data.type === "INSERT" && data.table === "DownloadTask") {
+    const log =
+      await $`gcloud beta batch jobs submit download-job-${createRandomString(
+        16
+      )} --location asia-southeast1 --config ./configs/download-job.json`;
+    return reply.code(200).send({ message: log.message, stdout: log.stdout });
+  }
+  return reply.code(401).send({ message: "invalid request" });
 });
 
-// app.post("/media", async (request, reply) => {
-//   const { data } = request.body as SupabaseWebhookData<{ url: string }>;
-//   if (data.type === "DELETE" && data.table === "Media") {
-//     data.old_record.url;
-//   }
-//
-//   reply.code(200).send({});
-// });
+app.post("/media", async (request, reply) => {
+  const data = request.body as SupabaseWebhookData<
+    null,
+    {
+      bucket: string;
+      key: string;
+      url: string;
+    }
+  >;
+  if (data.type === "DELETE" && data.table === "Media") {
+    await S3.send(
+      new DeleteObjectCommand({
+        Bucket: data.old_record.bucket,
+        Key: data.old_record.key,
+      })
+    );
+    reply.code(200).send({ message: `deleted media ${data.old_record.url}` });
+  }
+});
 
 app.listen(process.env.PORT || 3000, "0.0.0.0");
