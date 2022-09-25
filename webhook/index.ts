b6a650cf59c16698b7995f29badc49a2e36f9cba
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-09-25 15:38:07 +0900

    add docker-compose and cloudflared-tunnel

--- webhook/index.ts
+++ webhook/index.ts
@@ -12,6 +12,18 @@ const createRandomString = (length: number) => {
 
 const app = fastify({ logger: true });
 
+type SupabaseWebhookData<
+  T extends Record<string, unknown> = Record<string, any>
+> = {
+  data: {
+    type: "DELETE" | "INSERT" | "UPDATE";
+    table: string;
+    record: null | unknown;
+    schema: string;
+    old_record: T;
+  };
+};
+
 app.post("/download-task", async (request, reply) => {
   const log =
     await $`gcloud beta batch jobs submit download-job-${createRandomString(
@@ -19,5 +31,14 @@ app.post("/download-task", async (request, reply) => {
     )} --location asia-southeast1 --config ./configs/download-job.json`;
   reply.code(200).send({ message: log.message, stdout: log.stdout });
 });
+
+// app.post("/media", async (request, reply) => {
+//   const { data } = request.body as SupabaseWebhookData<{ url: string }>;
+//   if (data.type === "DELETE" && data.table === "Media") {
+//     data.old_record.url;
+//   }
+//
+//   reply.code(200).send({});
+// });
 
 app.listen(process.env.PORT || 3000, "0.0.0.0");
