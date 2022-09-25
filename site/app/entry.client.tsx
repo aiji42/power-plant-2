9d0991f554120dd2892d086abdfd44078b1784e2
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-09-25 22:28:50 +0900

    add chakra-ui

--- site/app/entry.client.tsx
+++ site/app/entry.client.tsx
@@ -1,4 +1,31 @@
 import { RemixBrowser } from "@remix-run/react";
 import { hydrateRoot } from "react-dom/client";
+import { CacheProvider } from "@emotion/react";
+import { ClientStyleContext } from "~/styles/context";
+import createEmotionCache from "~/styles/createEmotionCache";
+import { ReactNode, useCallback, useState } from "react";
 
-hydrateRoot(document, <RemixBrowser />);
+interface ClientCacheProviderProps {
+  children: ReactNode;
+}
+
+function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+  const [cache, setCache] = useState(createEmotionCache());
+
+  const reset = useCallback(() => {
+    setCache(createEmotionCache());
+  }, []);
+
+  return (
+    <ClientStyleContext.Provider value={{ reset }}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
+  );
+}
+
+hydrateRoot(
+  document,
+  <ClientCacheProvider>
+    <RemixBrowser />
+  </ClientCacheProvider>
+);
--- site/app/entry.server.tsx
+++ site/app/entry.server.tsx
@@ -1,6 +1,10 @@
 import type { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
+import { CacheProvider } from "@emotion/react";
+import createEmotionServer from "@emotion/server/create-instance";
+import { ServerStyleContext } from "~/styles/context";
+import createEmotionCache from "~/styles/createEmotionCache";
 
 export default function handleRequest(
   request: Request,
@@ -8,8 +12,25 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  const html = renderToString(
+    <ServerStyleContext.Provider value={null}>
+      <CacheProvider value={cache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </CacheProvider>
+    </ServerStyleContext.Provider>
+  );
+
+  const chunks = extractCriticalToChunks(html);
+
   const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
+    <ServerStyleContext.Provider value={chunks.styles}>
+      <CacheProvider value={cache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </CacheProvider>
+    </ServerStyleContext.Provider>
   );
 
   responseHeaders.set("Content-Type", "text/html");
