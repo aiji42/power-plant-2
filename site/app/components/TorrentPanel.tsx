2d84b7181c87af3781b4250042fc2b3b6742b3d4
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-10-10 02:50:57 +0900

    update torrent panel

--- site/app/components/TorrentPanel.tsx
+++ site/app/components/TorrentPanel.tsx
@@ -14,11 +14,19 @@ import {
   StatHelpText,
   StatLabel,
   useDisclosure,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+  Text,
 } from "@chakra-ui/react";
 import { useFetcher } from "@remix-run/react";
 import { SerializeFrom } from "@remix-run/node";
 import { loader as torrentLoader } from "~/routes/__authed/api/torrent.$code";
-import { useEffect } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 import { route } from "routes-gen";
 import { BiCheck, BiCloudDownload } from "react-icons/bi";
 import { FocusLock } from "@chakra-ui/focus-lock";
@@ -64,23 +72,82 @@ export const TorrentPanel = ({
             </Center>
           )}
           {torrentFetcher.data?.items.map((item) => (
-            <Stat key={item.link} my={4}>
-              <StatLabel noOfLines={2}>{item.title}</StatLabel>
-              <StatHelpText>
-                {item.registeredAt}
-                <br />
-                {item.size}
-                <StatArrow type="increase" ml={2} />
-                {item.seeder}
-                <StatArrow type="decrease" ml={2} />
-                {item.leech}
-                <Icon as={BiCheck} ml={2} />
-                {item.completed}
-              </StatHelpText>
-            </Stat>
+            <DownloadConfirm key={item.link} item={item}>
+              <Stat my={4}>
+                <StatLabel noOfLines={2}>{item.title}</StatLabel>
+                <StatHelpText>
+                  {item.registeredAt}
+                  <br />
+                  {item.size}
+                  <StatArrow type="increase" ml={2} />
+                  {item.seeder}
+                  <StatArrow type="decrease" ml={2} />
+                  {item.leech}
+                  <Icon as={BiCheck} ml={2} />
+                  {item.completed}
+                </StatHelpText>
+              </Stat>
+            </DownloadConfirm>
           ))}
+          {torrentFetcher.data && torrentFetcher.data.items.length < 1 && (
+            <Text>No data</Text>
+          )}
         </FocusLock>
       </PopoverContent>
     </Popover>
+  );
+};
+
+const DownloadConfirm = ({
+  children,
+  item,
+}: {
+  children: ReactNode;
+  item: SerializeFrom<typeof torrentLoader>["items"][number];
+}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef<any>(null);
+  return (
+    <>
+      <Box onClick={onOpen}>{children}</Box>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Download Confirm
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              <Stat>
+                <StatLabel>{item.title}</StatLabel>
+                <StatHelpText>
+                  {item.registeredAt} {item.size}
+                  <StatArrow type="increase" ml={2} />
+                  {item.seeder}
+                  <StatArrow type="decrease" ml={2} />
+                  {item.leech}
+                  <Icon as={BiCheck} ml={2} />
+                  {item.completed}
+                </StatHelpText>
+              </Stat>
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="teal" onClick={onClose} ml={3}>
+                OK
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 };
