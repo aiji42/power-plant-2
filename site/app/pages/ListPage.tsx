d766d48776cf2998c7109e48ab03128d7926c893
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-10-13 00:52:21 +0900

    update toolbar

--- site/app/pages/ListPage.tsx
+++ site/app/pages/ListPage.tsx
@@ -18,67 +18,23 @@ import {
   Image,
   Input,
   Spacer,
+  IconButton,
 } from "@chakra-ui/react";
 import { FaCircle } from "react-icons/fa";
-import { GrStackOverflow } from "react-icons/gr";
+import { GrStackOverflow, GrSearch } from "react-icons/gr";
 import { route } from "routes-gen";
 import { useSwipeToNext } from "~/hooks/useSwipeToNext";
 import { loader } from "~/routes/__authed/stock.$page";
 import { color } from "~/libs/status/utils";
-import { Fragment } from "react";
+import { ChangeEvent, Fragment, useReducer, useRef } from "react";
 
 export function ListPage() {
   const data = useLoaderData<typeof loader>();
-  const location = useLocation();
   const { handler, swiping } = useSwipeToNext(data.nextTo);
 
   return (
     <Box p={2} {...handler}>
-      <Box
-        boxShadow="dark-lg"
-        m={1}
-        mb={4}
-        p={4}
-        top={1}
-        position="sticky"
-        rounded="md"
-        bgColor="gray.900"
-        zIndex={10}
-      >
-        <Flex justify="space-between">
-          <Input w={32} rounded="3xl" bg="gray.200" />
-          <Spacer />
-          {(["mgs", "fana", "fanc"] as const).map((c) => (
-            <Fragment key={c}>
-              <Center>
-                <ChakraLink
-                  as={Link}
-                  to={route(`/${c}/:page`, { page: "1" })}
-                  color={
-                    location.pathname.startsWith(`/${c}`)
-                      ? "teal.300"
-                      : "gray.300"
-                  }
-                >
-                  {c}
-                </ChakraLink>
-              </Center>
-              <Spacer />
-            </Fragment>
-          ))}
-          <Center>
-            <ChakraLink
-              as={Link}
-              to={route("/stock/:page", { page: "1" })}
-              color={
-                location.pathname.startsWith("/stock") ? "teal.300" : "gray.300"
-              }
-            >
-              <Icon mt={2} as={GrStackOverflow} />
-            </ChakraLink>
-          </Center>
-        </Flex>
-      </Box>
+      <Toolbar />
       <Grid templateColumns="repeat(3, 1fr)" gap={2}>
         {data.items.map(
           ({ image_path: src, name, sku: code, casts, status }) => (
@@ -94,12 +50,11 @@ export function ListPage() {
                 {status && (
                   <Icon
                     as={FaCircle}
-                    color={color(status, 500)}
-                    top={1}
-                    right={1}
+                    color={color(status)}
+                    top={-2}
+                    left={-2}
                     position="absolute"
                     fontSize="md"
-                    boxShadow="base"
                   />
                 )}
               </GridItem>
@@ -116,3 +71,92 @@ export function ListPage() {
     </Box>
   );
 }
+
+const Toolbar = () => {
+  const location = useLocation();
+  const ref = useRef<HTMLInputElement>(null);
+  const [expand, handler] = useReducer((s: boolean, b: boolean) => {
+    if (ref.current && b) ref.current.focus();
+    return b;
+  }, false);
+  const [value, onChange] = useReducer(
+    (s: string, e: ChangeEvent<HTMLInputElement>) => e.target.value,
+    ""
+  );
+
+  return (
+    <Box
+      boxShadow="dark-lg"
+      m={1}
+      mb={4}
+      p={4}
+      top={1}
+      position="sticky"
+      rounded="md"
+      bgColor="gray.900"
+      zIndex={10}
+    >
+      <Flex justify="space-between">
+        <form
+          action={route("/product/:code", { code: value })}
+          style={{ display: "contents" }}
+        >
+          <Input
+            ref={ref}
+            w={expand ? 48 : 0}
+            opacity={expand ? 100 : 0}
+            onBlur={() => handler(false)}
+            rounded="3xl"
+            colorScheme="whiteAlpha"
+            transition="0.2s linear"
+            onChange={onChange}
+          />
+        </form>
+        <IconButton
+          mx={4}
+          left={0}
+          position="absolute"
+          rounded="3xl"
+          aria-label="Search"
+          icon={<GrSearch />}
+          opacity={!expand ? 100 : 0}
+          onClick={() => handler(true)}
+          colorScheme="telegram"
+          transition="0.2s linear"
+        />
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        {(["mgs", "fana", "fanc"] as const).map((c) => (
+          <Fragment key={c}>
+            <Center>
+              <ChakraLink
+                as={Link}
+                to={route(`/${c}/:page`, { page: "1" })}
+                color={
+                  location.pathname.startsWith(`/${c}`)
+                    ? "teal.300"
+                    : "gray.300"
+                }
+              >
+                {c}
+              </ChakraLink>
+            </Center>
+            <Spacer />
+          </Fragment>
+        ))}
+        <Center>
+          <ChakraLink
+            as={Link}
+            to={route("/stock/:page", { page: "1" })}
+            color={
+              location.pathname.startsWith("/stock") ? "teal.300" : "gray.300"
+            }
+          >
+            <Icon mt={2} as={GrStackOverflow} />
+          </ChakraLink>
+        </Center>
+      </Flex>
+    </Box>
+  );
+};
