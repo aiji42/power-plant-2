abe3096eeb9b87b7eb91bb1672b13435a8b98db3
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-10-13 23:22:17 +0900

    auto redirect on mismatch code

--- site/app/routes/__authed/product.$code.tsx
+++ site/app/routes/__authed/product.$code.tsx
@@ -1,5 +1,5 @@
-import { DataFunctionArgs, json } from "@remix-run/node";
-import { RouteParams } from "routes-gen";
+import { DataFunctionArgs, json, redirect } from "@remix-run/node";
+import { route, RouteParams } from "routes-gen";
 import { productFromMGS } from "~/libs/poduct/mgs.server";
 import { cacheHeaders } from "~/libs/cache/cache.server";
 import { productFromFAN } from "~/libs/poduct/fan.server";
@@ -13,6 +13,12 @@ export const loader = async ({ params }: DataFunctionArgs) => {
   ]);
   const product = mgs ?? fan;
   if (!product) throw new Response("", { status: 404 });
+
+  if (code !== product.code)
+    return redirect(route("/product/:code", { code: product.code }), {
+      status: 301,
+      headers: cacheHeaders(),
+    });
 
   return json(
     {
