44a022c1adb67becdceaa3a53c4b3c68a0c332cd
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-10-15 14:36:16 +0900

    update search torrent link and exclude duplicate

--- site/app/routes/__authed/api/torrent.$code.ts
+++ site/app/routes/__authed/api/torrent.$code.ts
@@ -2,12 +2,15 @@ import { DataFunctionArgs, json } from "@remix-run/node";
 import { RouteParams } from "routes-gen";
 import { formatter } from "~/libs/sku/sku";
 import { cacheHeaders } from "~/libs/cache/cache.server";
-import { searchLinks } from "~/libs/torrent/searchLinks";
+import { SearchedResult, searchLinks } from "~/libs/torrent/searchLinks";
 
 export const loader = async ({ params }: DataFunctionArgs) => {
   const { code } = params as RouteParams["/api/torrent/:code"];
-  const items = (await Promise.all(formatter(code).map((c) => searchLinks(c))))
+  const items = (await Promise.all(formatter(code).map(searchLinks)))
     .flat()
+    .reduce<SearchedResult[]>((res, item) => {
+      return res.some(({ link }) => link === item.link) ? res : [...res, item];
+    }, [])
     .sort(({ completed: a }, { completed: b }) =>
       Number(a) > Number(b) ? -1 : 1
     );
