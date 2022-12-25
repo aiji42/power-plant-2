5240264e832d79ed42f4e2c3f38540e390de3ef6
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-12-25 21:00:00 +0900

    Bumpup 2022-12-25

--- site/app/entry.client.tsx
+++ site/app/entry.client.tsx
@@ -2,7 +2,7 @@ import { RemixBrowser } from "@remix-run/react";
 import { hydrateRoot } from "react-dom/client";
 import { CacheProvider } from "@emotion/react";
 import { ClientStyleContext } from "~/styles/context";
-import createEmotionCache from "~/styles/createEmotionCache";
+import createEmotionCache, { defaultCache } from "~/styles/createEmotionCache";
 import { ReactNode, useCallback, useState } from "react";
 
 interface ClientCacheProviderProps {
@@ -10,7 +10,7 @@ interface ClientCacheProviderProps {
 }
 
 function ClientCacheProvider({ children }: ClientCacheProviderProps) {
-  const [cache, setCache] = useState(createEmotionCache());
+  const [cache, setCache] = useState(defaultCache);
 
   const reset = useCallback(() => {
     setCache(createEmotionCache());
--- site/app/styles/createEmotionCache.ts
+++ site/app/styles/createEmotionCache.ts
@@ -3,3 +3,5 @@ import createCache from "@emotion/cache";
 export default function createEmotionCache() {
   return createCache({ key: "css" });
 }
+
+export const defaultCache = createEmotionCache();
