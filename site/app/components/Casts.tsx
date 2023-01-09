07036a79b9655bde04cbe2de04f2ddf42cfc10d8
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-01-09 22:38:04 +0900

    update Casts

--- site/app/components/Casts.tsx
+++ site/app/components/Casts.tsx
@@ -1,6 +1,6 @@
 import { Link, useFetcher } from "@remix-run/react";
 import { loader as castsLoader } from "~/routes/__authed/api/casts.$code";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { route } from "routes-gen";
 import { Box, Skeleton, Tag, TagLabel, TagRightIcon } from "@chakra-ui/react";
 import { BiCheck, BiPlus } from "react-icons/bi";
@@ -8,24 +8,26 @@ import { useBookmarkProvider } from "~/components/BookmarkProvider";
 
 export const Casts = ({ code }: { code: string }) => {
   const castFetcher = useFetcher<typeof castsLoader>();
+  const { bookmark } = useBookmarkProvider();
   useEffect(() => {
     castFetcher.load(route("/api/casts/:code", { code }));
-  }, [castFetcher.load, code]);
-  const { bookmark } = useBookmarkProvider();
+  }, [castFetcher.load, code, bookmark?.casts?.length]);
 
-  let casts =
-    bookmark?.casts?.map(({ name, _count: { products: count } }) => ({
-      name,
-      count,
-      connected: true,
-    })) ?? [];
-  casts = (castFetcher.data?.casts ?? []).reduce(
-    (res, { name, productCount: count }) => {
-      if (res.some((item) => item.name === name)) return res;
-      return [...res, { name, count, connected: false }];
-    },
-    casts
-  );
+  const casts = useMemo(() => {
+    const casts =
+      bookmark?.casts?.map(({ name, _count: { products: count } }) => ({
+        name,
+        count,
+        connected: true,
+      })) ?? [];
+    return (castFetcher.data?.casts ?? []).reduce(
+      (res, { name, productCount: count }) => {
+        if (res.some((item) => item.name === name)) return res;
+        return [...res, { name, count, connected: false }];
+      },
+      casts
+    );
+  }, [bookmark?.casts, castFetcher.data?.casts]);
 
   return (
     <>
